d52a859ebe63cb17946867f6a4a960b35b19769a
Catherine Gloria <catherinegloria@gmail.com>
2020-09-04 02:50:34 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -27,7 +27,6 @@ def printer(attendees)
   badges_array.map do |greeting|
     puts badges_array.first
   end
-  #make an array of badges that we iterate over
   room_array = assign_rooms(attendees)
   room_array.map do |room|
     puts room_array.first
