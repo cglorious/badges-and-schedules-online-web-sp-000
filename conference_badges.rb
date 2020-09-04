4313d0a600b10d367f353d9b6b7a0a7b0a47f160
Catherine Gloria <catherinegloria@gmail.com>
2020-09-04 02:09:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,33 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speakers)
+  speakers.map do |name_array|
+    "Hello, my name is #{name_array}."
+  end
+end
+
+def assign_rooms(assign_array)
+  new_assign_array = []
+  assign_array.each_with_index do |name, room|
+    number = room + 1
+    greeting = "Hello, #{name}! You'll be assigned to room #{number}!"
+    new_assign_array << greeting
+  end
+  new_assign_array
+end
+
+def printer(speaker_array)
+  speaker_array.each do |greeting|
+    hello = batch_badge_creator(greeting)
+    puts hello[0]
+  end
+  speaker_array.each do |name_and_room|
+    room = assign_rooms(name_and_room)
+    puts room[0]
+  end
+end
+
+#currently returns two arrays
+#what method returns values?
