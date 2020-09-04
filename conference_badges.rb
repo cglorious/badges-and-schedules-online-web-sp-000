d23ef28fae3add1300e5d4e27dd3aae7332c0aad
Catherine Gloria <catherinegloria@gmail.com>
2020-09-04 02:27:39 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,16 +19,17 @@ def assign_rooms(assign_array)
 end
 
 def printer(attendees)
-# attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   attendees.each do |greeting|
     hello = batch_badge_creator(greeting)
     puts hello.first
   end
-#  attendees.each do |room|
-#    key = assign_rooms(room)
-#    puts key.first
-#  end
+  attendees.each do |room|
+    key = assign_rooms(room)
+    puts key.first
+  end
 end
+
+# attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 #currently returns two arrays
 #what method returns values?
